f47a79c2b47275a7f5eedef068dd093714282ba1
Hyun Jae Stephen Chu <stephenchu95@gmail.com>
2025-07-14 16:44:44 +0900

    fig

--- TS_lib/save_as_html.m
+++ TS_lib/save_as_html.m
@@ -19,20 +19,17 @@ function save_as_html(figure,folder,filename,option)
 % July 8th, 2025
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 p = fig2plotly(figure, 'offline', true, 'filename', fullfile(folder, filename), 'open', false);
-if nargin == 4
-p.layout.legend.orientation = 'h';
-% p.layout.legend.x = 0.36;
-% p.layout.legend.y = 0.84;
-% p.layout.width = 700;
-% p.layout.height = 450;
-% p.layout.margin = struct('t', 30, 'l', 5, 'r', 5, 'b', 10);
-
-p.layout.legend.x = option.legx;
-p.layout.legend.y = option.legy;
-p.layout.width = option.width;
-p.layout.height = option.height;
-p.layout.margin = option.margin;
-
+if length(fieldnames(option)) < 4
+    p.layout.width = option.width;
+    p.layout.height = option.height;
+    p.layout.margin = option.margin;
+else
+    p.layout.width = option.width;
+    p.layout.height = option.height;
+    p.layout.margin = option.margin;
+    p.layout.legend.orientation = option.legori;
+    p.layout.legend.x = option.legx;
+    p.layout.legend.y = option.legy;
 end
 
 plotlyoffline(p);
