868e6f28ba9145d416c6f83824a464dab15a161b
Hyun Jae Stephen Chu <stephenchu95@gmail.com>
2025-07-29 16:24:08 +0900

    ex

--- Exercises/ex3_2_AR.m
+++ Exercises/ex3_2_AR.m
@@ -1,13 +1,9 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% This exercise plots the macro data of Korea and the US, computes the 
-% autocovariance functions, and tests the autocorrelation functions
-% (check if white noise)
-
-% The critical value of the build-in function regarding the autocorrelation
-% function is 2 standard error limits not 1.96.
+% This exercise estimates and compares the AR(p) process of the US
+% macroeconomic data using OLS and MLE.
 
 % Written by Hyun Jae Stephen Chu
-% August 6th, 2024
+% July 29th, 2025
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% 0. Settings
 clear;clc;
@@ -16,14 +12,6 @@ rng(123)
 
 addpath("/Users/hjchu/Documents/GitHub/Time_Series_Package/Exercises")
 addpath(genpath("/Users/hjchu/Documents/GitHub/Time_Series_Package/TS_lib"))
-
-save_folder = 'ex3_1_results';
-
-fig_option = struct( ...
-    'width', 700, ...
-    'height', 400, ...
-    'margin', struct('t', 30, 'l', 5, 'r', 5, 'b', 0) ...
-    );
 
 %% 1. Loading Data
 KOR_data = readmatrix("tsdata_20250704.xlsx",'Sheet','KOR','Range','B2:M102');
@@ -72,28 +60,10 @@ kor_gov1Y = kor_int(:,3);
 kor_gov3Y = kor_int(:,4);
 % 1960 Q2 - 2025 Q1
 
-%% Autocovariance of Variables
-us_y = [us_growth,us_gdpdef_inf,us_pce_inf,us_cpi_inf,us_ffe,us_T10Y,us_unemp];
-us_var_list = {'growth','gdpdefinf','pceinf','cpiinf','ffe','T10Y','unemp'};
-ind_control = 4;
-us_var = us_y(:,ind_control);
+%% 3. AR OLS
+p = 4;
+H = 8;
+% [phi_hat, sig2_hat, F, Y0, Y_lag, y_hat, u_hat, Y_predm] = OLS_ARp(kor_var,p,H);
+[phi_hat2, sig2_hat2, F2, Y02, Y_lag2, y_hat2, u_hat2, Y_predm2] = OLS_ARp(us_var,p,H);
 
-kor_y = [kor_growth,kor_gdpdef_inf,kor_cpi_inf,kor_forex_growth,kor_call,kor_cd,kor_gov1Y,kor_gov3Y,kor_unemp];
-kor_var_list = {'growth', 'gdpdefinf','cpiinf','dforex','call','cd91','gov1','gov3','unemp'}';
-ind_control = 1; % Use this as the control of choosing which variable to use
-kor_var = kor_y(:,ind_control);
-
-[acor1,bound1] = autocor(us_var,[],1);
-f1 = gcf;
-save_as_html(f1,save_folder,'figure1',fig_option)
-
-[acor2,bound2] = autocor(us_y(:,1),[],1);
-f2 = gcf;
-save_as_html(f2,save_folder,'figure2',fig_option)
-
-[acor3,bound3] = autocor(kor_var,[],1);
-
-%% BP and LB Test
-[BoxPierce1, pval_BP1, LjungBox1, pval_LB1, tb1] = WN_test(us_var);
-[BoxPierce2, pval_BP2, LjungBox2, pval_LB2, tb2] = WN_test(us_y(:,1));
-[BoxPierce3, pval_BP3, LjungBox3, pval_LB3, tb3] = WN_test(kor_var);
+%% 4. AR MLE
